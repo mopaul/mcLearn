32991bae5e85e59c358157956c3491c7c46ae31d
Moutupsi <moutupsi.paul@stonybrook.edu>
2013-11-14 06:52:41 +0100

    Add a pause after 3d surface plotted

--- mlclass-ex1/ex1.m
+++ mlclass-ex1/ex1.m
@@ -112,7 +112,7 @@ J_vals = J_vals';
 figure;
 surf(theta0_vals, theta1_vals, J_vals)
 xlabel('\theta_0'); ylabel('\theta_1');
-
+pause
 % Contour plot
 figure;
 % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
